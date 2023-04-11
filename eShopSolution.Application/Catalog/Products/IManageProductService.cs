e712f246c80df92cb3c9315368acef163202bdd4
fomust1969@gmail.com <fomust1969@gmail.com>
2023-04-11 13:37:14 +0700

    Add project files.

---
+++ eShopSolution.Application/Catalog/Products/IManageProductService.cs
@@ -0,0 +1,44 @@
+﻿
+using eShopSolution.ViewModels.Catalog.Product;
+using eShopSolution.ViewModels.Common;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using eShopSolution.ViewModels.Catalog.ProductImages;
+using Microsoft.AspNetCore.Http;
+
+namespace eShopSolution.Application.Catalog.Products
+{
+    //interface
+    public interface IManageProductService
+    {
+        // dinh nghia phuong thuc
+        //tra ve ma san pham vua tao
+        Task<int> Create(ProductCreateRequest request);
+
+        Task<int> Update(ProductUpdateRequest request);
+
+        Task<int> Delete(int productId);
+        Task<ProductViewModel>GetById(int productId, string languageId); //
+
+        Task<bool> UpdatePrice(int productId, decimal newPrice);
+
+        Task<bool> UpdateStock(int productId, int addedQuantity);
+        Task AddViewcount(int productId);
+
+        Task<int> AddImage(int productId, List<IFormFile> files); //, ProductImageCreateRequest request
+
+        Task<int> UpdateImage(int imageId, ProductImageUpdateRequest request);
+
+        Task<ProductImageViewModel> GetImageById(int imageId);
+
+        Task<List<ProductImageViewModel>> GetListImages(int productId);
+
+        Task<int> RemoveImage(int imageId);
+        //truyen 1 so request 
+        Task<PagedResult<ProductViewModel>> GetAllPaging(GetManageProductPagingRequest request);
+    }
+}
---
+++ eShopSolution.ViewModels/Catalog/Product/GetPublicProductPagingRequest.cs
@@ -0,0 +1,16 @@
+﻿// using eShopSolution.Application.Dtos;
+using eShopSolution.ViewModels.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eShopSolution.ViewModels.Catalog.Product
+{
+    public class GetPublicProductPagingRequest : PagingRequestBase
+    {
+        public string languageId { get; set; }
+        public int? CategoryId { get; set; }
+    }
+}
